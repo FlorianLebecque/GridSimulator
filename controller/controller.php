a6eecd5c35b4f6caa06dd75547d21060edd8b361
Florian <Florianlebecque@hotmail.com>
2020-12-05 15:04:47 +0100

    clear controller php

--- controller/controller.php
+++ controller/controller.php
@@ -2,19 +2,14 @@
     session_start();
 
     include_once("backend/header.php");
-    include_once("backend\inc\graph.php");
+    
 
     class Controller {  
          
         public $hdrData;
-        public $grahpManager;
-        public $status;
 
         public function __construct(){    
            $this->hdrData = new headerData();
-           $this->grahpManager = new graphHandler();
-
-           $this->status = false;
         } 
 
 
