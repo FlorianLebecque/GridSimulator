1f7d93b2b5f06df073ebd0cb3e06f5a121ba024d
Florian <Florianlebecque@hotmail.com>
2020-12-07 15:55:25 +0100

    start working on editor again

---
+++ frontend/page/editor.php
@@ -0,0 +1,18 @@
+
+<div class="row wideContainer">
+    <div class="col-sm-12">
+        <div class="main">
+            <button class="collapsible">Node editor</button>
+            <div class="collasibleContent"><hr>
+                <div class="row">
+                    <div class="col-12">
+                       
+                            <button class="btn">add</button>
+
+                        
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </div>
+</div>
