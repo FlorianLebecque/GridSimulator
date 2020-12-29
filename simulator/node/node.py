edc322a0fcc0524bd581f78c5de3bc024b5ffddc
Lebecque <Florianlebecque@hotmail.com>
2020-12-29 16:54:01 +0100

    add strat exception

--- simulator/node/node.py
+++ simulator/node/node.py
@@ -45,21 +45,32 @@ class Node:
             #function that find all the node wich have the selected attribute
     
 
-    def TryStrat(self,strat,bill):
+    def TryStrat(self,strat,excepts):
+
         for s in strat:
-            node_id = self.TryAttribute(s,bill)
+            node_id = self.TryAttribute(s[0],s[1],excepts)
             print("strat : ",s, "node : ",node_id)
             if node_id != -1:
-                break
+                return (s[0],node_id)
         
-    def TryAttribute(self,attr,param):
+        return -1
+
+    def TryAttribute(self,attr,param,excepts):
         target_node = self.getNodeArray(attr)
         node_id = -1
+        
         for child in target_node:
-            attr_func = getattr(child,attr)
-            node_id = attr_func(param)
-            if node_id != -1:
-                return node_id
+            alreadytry = False
+            
+            for ex in excepts:
+                if ex[0] == attr and child._id == ex[1]:
+                    alreadytry = True
+            
+            if not alreadytry:
+                attr_func = getattr(child,attr)
+                node_id = attr_func(param)
+                if node_id != -1:
+                    return node_id
 
         return node_id
 
@@ -77,3 +88,15 @@ class Node:
 
         #we need to sort by prior
         return sorted(result_node, key=lambda x: x.prior, reverse=False)
+
+    def setUserEnable(self,node_id,en):
+        if self._id == node_id:
+            self.userEnable = en
+            return self._id
+        else:
+            for child in self.childs:
+                node = child.setUserEnable(node_id,en)
+                if node != -1:
+                    return node
+        
+        return -1
