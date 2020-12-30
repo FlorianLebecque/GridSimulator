f6b95d1289d591f99d1f3079b13de618c46afb4f
Lebecque <Florianlebecque@hotmail.com>
2020-12-30 18:24:21 +0100

    Add randomness function

--- simulator/node/NodeC.py
+++ simulator/node/NodeC.py
@@ -1,7 +1,14 @@
 from node.node import Node
+import random
 class NodeC(Node):
     def __init__(self, _id, ligne_pwr):
         super().__init__( _id, ligne_pwr)    
+
+    def getRandomArray(self):
+        temp = []
+        for i in range(0,48):
+            temp.append(random.uniform(-self.max_power*0.1,self.max_power*0.1))
+        return temp
 
     def disable_cons(self,b):
         if self.enable:
