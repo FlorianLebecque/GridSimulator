38f8ec745d10974ea52da3b6dba84cea83112134
Lebecque <Florianlebecque@hotmail.com>
2021-01-02 12:08:39 +0100

    add field on load

--- frontend/JS/editor.js
+++ frontend/JS/editor.js
@@ -1,15 +1,24 @@
 //fill the node type in the correct select
 $("#prd_nodeType").html(setType_option(array_DataTypeField, "p"))
 $("#cns_nodeType").html(setType_option(array_DataTypeField, "c"))
+
 $("#prd_nodeType").change(array_DataTypeField,get_form_field);
 $("#cns_nodeType").change(array_DataTypeField,get_form_field);
+
+$("#prd_nodeType").change()
+$("#cns_nodeType").change()
 
 function setType_option(array_DataTypeField, simple_type){
     let html = "";
     
     for(let i = 0; i < array_DataTypeField.length;i++){
         if(array_DataTypeField[i]["simple_type"] == simple_type){
-            html += "<option value='"+array_DataTypeField[i]["type"]+"'>"+ array_DataTypeField[i]["label"] +"</option>";
+            if(i == 0) {
+                html += "<option selected value='"+array_DataTypeField[i]["type"]+"'>"+ array_DataTypeField[i]["label"] +"</option>";
+            }else{
+                html += "<option value='"+array_DataTypeField[i]["type"]+"'>"+ array_DataTypeField[i]["label"] +"</option>";
+            }
+            
         }
     }
 
@@ -19,7 +28,7 @@ function setType_option(array_DataTypeField, simple_type){
 function getDataTypeFiled_by_type(target_type,array_DataTypeField){
 
     for(let i = 0; i < array_DataTypeField.length;i++){
-        if(array_DataTypeField[i]["type"] == target_type){
+        if(array_DataTypeField[i]["type"] == target_type.toString()){
             return array_DataTypeField[i];
         }
     }
